4c24965240f9e692990a7547c30d6cc5d78f09f6
CleanSand <kagami228k@mail.ru>
2023-11-24 16:15:09 +0700

    ReportWindow

---
+++ ISFATAAIOWATE/Views/ReportsWindow.axaml.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ISFATAAIOWATE.Views;
+
+public partial class ReportsWindow : Window
+{
+    public ReportsWindow()
+    {
+        InitializeComponent();
+#if DEBUG
+        this.AttachDevTools();
+#endif
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
